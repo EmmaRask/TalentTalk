f785fcc8809d2a1d7e61f844c285ee30a2c9b9ea
alebri0616 <alebri0616@skola.goteborg.se>
2026-04-09 22:04:09 +0200

    imported role button from components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@
 import { useNavigate } from "react-router-dom";
 import { text } from "../data/text";
 import { useMingel } from "../context/MingelContext";
+import RoleButton from "../components/RoleButton";
 
 export default function HomePage() {
     const navigate = useNavigate();
@@ -16,9 +17,9 @@ export default function HomePage() {
       <h1>{text.home.title}</h1>
       <p>{text.home.subtitle}</p>
 
-      <Company >
-
-      </Company>
+      <RoleButton onSelect={handleSelect} value="student">
+        {text.role.student}
+      </RoleButton>
 
      <button onClick={() => handleSelect('company')}>
         {text.role.company}
