9868905c478ff55f005134450f4fc4a31734516a
alebri0616 <alebri0616@skola.goteborg.se>
2026-04-10 00:07:54 +0200

    added continue button to home page and made it possible to select

--- src/components/RoleButton.jsx
+++ src/components/RoleButton.jsx
@@ -1,14 +1,17 @@
 export default function RoleButton({ onSelect, value, children, selected }) {
   return (
-    <button onClick={() => onSelect(value)}
-     style={{
-        border: selected ? "2px solid #132b47" : "1px solid #ccc",
-        background: selected ? "#E8F1FF" : "white",
-        padding: "10px 16px",
-        borderRadius: "6px",
+    <button
+      onClick={() => onSelect(value)}
+      style={{
+        padding: "12px 18px",
+        borderRadius: "8px",
+        backgroundColor: selected ? "#4A90E2" : "#ffffff",
+        color: selected ? "white" : "black",
+        border: selected ? "2px solid #4A90E2" : "1px solid #ccc",
         cursor: "pointer",
+        transition: "0.2s ease",
       }}
->
+    >
       {children}
     </button>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,27 +2,42 @@ import { useNavigate } from "react-router-dom";
 import { text } from "../data/text";
 import { useMingel } from "../context/MingelContext";
 import RoleButton from "../components/RoleButton";
+import { useState } from "react";
+import ContinueButton from "../components/ContinueButton";
+
+
+
 
 export default function HomePage() {
     const navigate = useNavigate();
     const { chooseUserType } = useMingel();
+    const [selectedRole, setSelectedRole] = useState(null);
     function handleSelect(type) {
-        chooseUserType(type);
-        navigate("/roll");
+        setSelectedRole(type);
     }
+      function handleContinue() {
+    chooseUserType(selectedRole);
+    navigate("/roll");
+  }
+
   return (
     <main>
       <h1>{text.home.title}</h1>
       <p>{text.home.subtitle}</p>
 
-      <RoleButton onSelect={handleSelect} value="company">
+      <RoleButton onSelect={handleSelect} value="company"  selected={selectedRole === "company"}
+>
         {text.role.company}
       </RoleButton>
 
-     <RoleButton onSelect={handleSelect} value="student">
+     <RoleButton onSelect={handleSelect} value="student" selected={selectedRole === "student"}
+>
         {text.role.student}
       </RoleButton>
-
+        <ContinueButton
+        disabled={!selectedRole}
+        onContinue={handleContinue}
+      />
     </main>
   );
 }
