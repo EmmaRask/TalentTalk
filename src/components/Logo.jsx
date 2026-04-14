77e9b94e9d2bf6a110921a5c09f472446908d493
EmmaRask <emmarask@yahoo.com>
2026-04-14 18:36:43 +0200

    made yrgo logo homebutton

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
   return (
-    <img
-      src="/media/icons/YrgoLogo.png"
-      alt="Yrgo logo"
-      className="app-logo"
-    />
+    <Link to="/" className="logo-link">
+      <img src="/media/icons/YrgoLogo.png" alt="YrgoLogo" />
+    </Link>
   );
 }
+
+
+// export default function Logo() {
+//   return (
+//     <img
+//       src="/media/icons/YrgoLogo.png"
+//       alt="Yrgo logo"
+//       className="app-logo"
+//     />
+//   );
+// }
